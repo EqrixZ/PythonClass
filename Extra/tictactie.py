ae271442ead65b5998c4485b8b00a3c9f5632ec4
EqrixZ <s6606022630049@email.kmutnb.ac.th>
2023-10-02 10:34:04 +0700

    Ex

---
+++ Extra/tictactie.py
@@ -0,0 +1,10 @@
+Board = [["#","#","#"],
+       ["#","#","#"],
+       ["#","#","#"],]
+for row in Board:
+    print(row)
+
+# def playervictory(inp):
+#     if (Board[0] == Board[1] == Board[2]) \
+#         or (Board[3] == Board[4] == Board[5]) \
+#         or (Board[6] == Board[7] == Board[8]):
